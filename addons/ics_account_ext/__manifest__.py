c41841b1b2438c8e0f15e22e1451f2c0db1e95f1
modnoting <arsachi99@gmail.com>
2022-07-05 09:28:04 +0700

    [ADD]add field res partner in addons ics_account_ext

---
+++ addons/ics_account_ext/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Incosource Account Ext',
+    'summary': '''
+        Account Function Ext''',
+    'author': 'Santi T.',
+    'website': 'https://roots.tech',
+    'version': '15.0.0.0.1',
+    'depends': [
+        'ics_core_update',
+        'sale',
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/sale_menuitem.xml',
+        # 'views/shop_type_view.xml',
+        # 'views/distribution_view.xml',
+        # 'views/sale_office_view.xml',
+        # 'views/sale_order_views.xml',
+        # 'views/res_partner_views.xml',
+        # 'views/sale_report_view.xml',
+    ],
+    'license': 'LGPL-3',
+}
