ba6417fe53055f82198c54a1542332973f258efe
xos <tewtawat@gmail.com>
2022-07-13 11:56:51 +0700

    [IMP] sale order - change group partner attribute and label

--- addons/ics_sale_ext/report/sale_report.py
+++ addons/ics_sale_ext/report/sale_report.py
@@ -13,3 +13,13 @@ class SaleReport(models.Model):
         string='Sale Office',
         comodel_name='sale.office',
     )
+
+    team_id = fields.Many2one(
+        string='Province',
+        comodel_name='crm.team',
+    )
+
+    industry_id = fields.Many2one(
+        string='Shop Type',
+        comodel_name='res.partner.industry',
+    )
