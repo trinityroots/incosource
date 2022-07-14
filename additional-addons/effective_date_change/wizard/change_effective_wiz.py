322242fc6aa6c44f322637cfefb353d47e98fc47
xos <tewtawat@gmail.com>
2022-07-15 00:32:38 +0700

    [IMP] change effective date - re sequence after change date

--- additional-addons/effective_date_change/wizard/change_effective_wiz.py
+++ additional-addons/effective_date_change/wizard/change_effective_wiz.py
@@ -99,6 +99,12 @@ class ChangeEffectiveWiz(models.TransientModel):
                         concat_pulled_name)
                     _logger.debug("Successfully changed account_move")
 
+                    journal_entry_id = self.env['account.move'].search([
+                        ('ref', 'ilike', concat_pulled_name)
+                    ])
+
+                    journal_entry_id._set_next_sequence()
+
                     # Update valuation
                     do_update(
                         query.update_stock_valuation,
@@ -178,6 +184,12 @@ class ChangeEffectiveWiz(models.TransientModel):
                             self.effective_date,
                             percentage_stock_picking)
                         _logger.debug("Successfully changed account_move")
+
+                        for picking_name in pulled_stock_picking:
+                            journal_entry_id = self.env['account.move'].search([
+                                ('ref', 'ilike', picking_name)
+                            ])
+                            journal_entry_id._set_next_sequence()
 
                         # Update journal items
                         do_update(
